1e8ea7ca82139ab53d91b5ae3a00f449188de036
Jesse James & ian bevel <student@epicodus.com>
2015-05-13 10:18:58 -0700

    create recipes.erb, add content to it, update app.rb with path info

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
-Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 require('bundler/setup')
 Bundler.require(:default)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
   erb(:index)
+end
+
+get('/recipes') do
+  @recipes = Recipe.all()
+  erb(:recipes)
+end
+
+post('/recipes/new') do
+  recipe_name = params.fetch('name')
+  instructions = params.fetch('instructions')
+  rating = params.fetch('rating').to_i()
+  # new_recipe = Recipe.new({:name => recipe_name, :instructions => instructions, :rating => rating})
+  # new_recipe.save() # the create will save automatically
+  new_recipe = Recipe.create({:name => recipe_name, :instructions => instructions, :rating => rating})
+  redirect("/recipes")
 end
