e9a0bf2e1efdcb1ce8e8941fdc8fb151a6751a25
Jesse James & ian bevel <student@epicodus.com>
2015-05-13 15:49:41 -0700

    update specs to pass with validation requirements, add validation spec to test instructions presence

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -3,4 +3,5 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :ingredients
 
   validates(:name, {:presence => true, :length => { :minimum => 1 }})
+  validates(:instructions, {:presence => true, :length => { :minimum => 1 }})
 end
--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -4,17 +4,17 @@ describe('Recipe') do
 
   describe('.sort_by') do
     it('sorts the array of recipes, by recipe rating we hope') do
-      test_1 = Recipe.create({:name => 'test1', :rating =>1 })
-      test_2 = Recipe.create({:name => 'test2', :rating =>3 })
-      test_3 = Recipe.create({:name => 'test3', :rating =>2 })
+      test_1 = Recipe.create({:name => 'test1', :rating =>1, :instructions => "cook" })
+      test_2 = Recipe.create({:name => 'test2', :rating =>3, :instructions => "cook" })
+      test_3 = Recipe.create({:name => 'test3', :rating =>2, :instructions => "cook" })
       sorted_recipes = Recipe.all().sort_by{ |recipe| recipe.rating() }
       expect(sorted_recipes).to(eq([test_1, test_3, test_2]))
     end
 
     it('sorts the array of recipes, by recipe rating in REVERSE') do
-      test_1 = Recipe.create({:name => 'test1', :rating =>1 })
-      test_2 = Recipe.create({:name => 'test2', :rating =>3 })
-      test_3 = Recipe.create({:name => 'test3', :rating =>2 })
+      test_1 = Recipe.create({:name => 'test1', :rating =>1, :instructions => "cook" })
+      test_2 = Recipe.create({:name => 'test2', :rating =>3, :instructions => "cook" })
+      test_3 = Recipe.create({:name => 'test3', :rating =>2, :instructions => "cook" })
       sorted_recipes = Recipe.all().sort_by{ |recipe| recipe.rating() }
       expect(sorted_recipes.reverse()).to(eq([test_2, test_3, test_1]))
     end
@@ -23,12 +23,12 @@ describe('Recipe') do
       recipe = Recipe.new({:name => ""})
       expect(recipe.save()).to(eq(false))
     end
-    # it('validates the presence of a recipe name') do
-    #   recipe = Recipe.new({:name => ""})
-    #   expect(recipe.save()).to(eq(false))
-    # end
-    # it('validates the presence of a recipe name') do
-    #   recipe = Recipe.new({:name => ""})
+    it('validates the presence of a recipe instructions') do
+      recipe = Recipe.new({:instructions => ""})
+      expect(recipe.save()).to(eq(false))
+    end
+    # it('validates the presence of a recipe rating') do
+    #   recipe = Recipe.new({:rating => ""})
     #   expect(recipe.save()).to(eq(false))
     # end
 
